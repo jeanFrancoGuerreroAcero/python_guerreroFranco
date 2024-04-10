4f0b3338474818b5a01abfc1ee4e6755717bfe11
jeanFrancoGuerreroAcero <166477787+jeanFrancoGuerreroAcero@users.noreply.github.com>
2024-04-10 12:59:58 -0500

    Add files via upload

---
+++ fibonacii_guerreroFranco.py
@@ -0,0 +1,17 @@
+print("bienvenido")
+print("la secuencia fibonacci se basa en el numero dado se suma con el mismo, el numero dado se suma con el anterior")
+print("vamos a hacer una secuencia fibonacci")
+print("hasta que numero quieres que llegue la secuencia")
+cant=input(int())
+p=0
+e=1
+c=0
+for i in range(i,cant):
+    while p>=89:
+     print(p)
+    break
+p=e
+b=c
+c=p+e
+    
+
