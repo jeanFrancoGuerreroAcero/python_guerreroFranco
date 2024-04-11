b11f82837ae25663d121876f2e74ac3761974753
jeanFrancoGuerreroAcero <166477787+jeanFrancoGuerreroAcero@users.noreply.github.com>
2024-04-11 12:57:58 -0500

    dia3

---
+++ dia3/ejercicio1_guerreroFranco.py
@@ -0,0 +1,15 @@
+def numPrim():
+    print("hola bienvenido")
+    print("vamos a ver si el numero que ingresas es primo")
+    print("recuerda que los numeros primos son aquellos que solo son divisbles por uno y por el mismo")
+    print("")
+
+p=print(int(input("escribe in numero")))
+
+if p%p==1:
+ print("este es un numero primo")
+else:
+   print("el numero que escribio no es primo")
+   print(int(input("ingrese un numero nuevamente")))
+   
+   
